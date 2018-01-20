95a9b26f2f758659a1a4290bb5a50cefd6d548e9
thEpisode <camiepisode@outlook.com>
2018-01-20 14:35:30 -0500

    saving name in localstorage

--- Client/app/src/main/java/com/bioverflow/dobermann/client/MainActivity.java
+++ Client/app/src/main/java/com/bioverflow/dobermann/client/MainActivity.java
@@ -5,17 +5,14 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
     private AlertDialog enableNotificationListenerAlertDialog;
 
     public  static ClientSettings clientSettings;
+
+    private EditText clientNameInput;
+    private Button setNameButton;
+    private SharedPreferences sharedPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +38,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void startApp(){
-        this.clientSettings = new ClientSettings("TATIANA");
+        /// UI definitions
+        this.clientNameInput = (EditText)findViewById(R.id.client_name_input);
+        this.setNameButton = findViewById(R.id.set_name_button);
 
-        final Button button = findViewById(R.id.start_notification_button);
-        button.setOnClickListener(new View.OnClickListener() {
+        /// Client settings definition
+        this.sharedPreferences = getSharedPreferences("ClientSettings", this.MODE_PRIVATE);
+        this.clientSettings = new ClientSettings(this.sharedPreferences.getString("Name", ""));
+
+        /// UI Event listeners
+        this.setNameButton .setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                showToast("Hello toast!");
+                clientSettings = new ClientSettings(clientNameInput.getText().toString());
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("Name", clientSettings.getName());
+                editor.commit();
             }
         });
 
